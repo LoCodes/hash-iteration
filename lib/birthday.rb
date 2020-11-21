ef88558577ec450aa7fbef92ff117b3c1055cf32
Loren Cipriano <loren25cipriano@gmail.com>
2020-11-20 18:47:23 -0800

    commit tree 90565d15797c6d7c16442d6c6b1995d432ab5d59

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,12 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each do |name,age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end 
 end
 
 
+
+# #happy_birthday
+# prints out a birthday message to each kid in the birthday_kids hash 
